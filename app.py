8dff761f5a8c018d1bee9d7476b8e1797ee9f052
Adnan Anwar Rabbani <adnananwarrabbani@gmail.com>
2026-08-30 11:20:40 +0700

    init

---
+++ app.py
@@ -0,0 +1,108 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+
+# Title and headers
+st.title("Main Title")
+st.header("Section Header")
+st.subheader("Subsection")
+
+# Body text
+st.write("Auto-formats anything!")
+st.markdown("**Bold**, *italic*, `code`")
+st.latex(r"E = mc^2")
+st.code("print('hello')", language="python")
+st.caption("Small grey caption")
+
+df = pd.DataFrame({'Name': ['Alice', 'Bob'], 'Score': [92, 85]})
+
+st.dataframe(df) # Interactive – sortable, scrollable
+st.table(df) # Static – full display
+st.metric("Accuracy", "92%", "+2%")  # KPI card with delta
+st.json({"model": "XGBoost", "accuracy": 0.92})
+
+rng = np.random.default_rng(42)
+dates = pd.date_range("2024-01-01", periods=60, freq="D")
+data = pd.DataFrame(
+{
+    "date": dates,
+    "product_A": rng.integers(50, 200, size=60).cumsum(),
+    "product_B": rng.integers(30, 150, size=60).cumsum(),
+    "product_C": rng.integers(20, 100, size=60).cumsum(),
+}
+).set_index("date")
+
+# line chart
+st.title("Sales Trends Over Time")
+st.line_chart(data)
+
+# bar chart
+st.title("Total Sales by Product")
+total_sales = data.sum()
+st.bar_chart(total_sales)
+
+# pie chart
+st.title("(plotly) Sales Share by Product")
+fig_pie = px.pie(
+    values=total_sales.values,
+    names=total_sales.index,
+    title="Total Sales Distribution",
+)
+st.plotly_chart(fig_pie)
+
+# plotly line chart
+st.title("(plotly) Sales Trends Over Time")
+fig_line = px.line(
+    data.reset_index(),
+    x="date",
+    y=["product_A", "product_B", "product_C"],
+    title="Sales Trends (Plotly)",
+)
+st.plotly_chart(fig_line)
+
+# Text inputs
+name = st.text_input("Your name")
+bio = st.text_area("About you")
+
+# Numeric inputs
+age = st.number_input("Age", 18, 100)
+rating = st.slider("Rating", 1, 10, 7)
+
+# Selection inputs
+model = st.selectbox("Model", ["RF", "XGBoost", "SVM"])
+features = st.multiselect("Features", ["Age", "Income", "Education"])
+
+# Boolean & file
+show = st.checkbox("Show raw data")
+file = st.file_uploader("Upload CSV")
+
+# Action
+if st.button("Train Model"):
+    st.write("Training...") #will print when button is clicked
+    
+
+# Sidebar
+with st.sidebar:
+    page = st.radio("Go to", ["Home", "Projects", "Contact"])
+
+# Columns
+col1, col2, col3 = st.columns(3)
+col1.metric("Accuracy", "92%")
+
+# Tabs
+tab1, tab2 = st.tabs(["EDA", "Model"])
+with tab1:
+    st.write("EDA content...")
+
+# Expander (collapsible)
+with st.expander("Show code"):
+    st.code("model.fit(X, y)")
+    
+    
+st.set_page_config(
+    page_title="Farrel Portfolio",
+    page_icon="🔥",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
